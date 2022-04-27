bc0ae75c722d392d908a1fed9d19cc41b223bb23
plasticine9750 <975036719@qq.com>
2022-04-27 09:27:20 +0800

    (problem) 搜索插入位置

---
+++ leetcode/35.搜索插入位置.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=typescript
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+function searchInsert(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    const num = nums[mid];
+
+    if (target < num) {
+      right = mid - 1;
+    } else if (target > num) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  // return target < nums[right] ? right : right + 1;
+  return left;
+}
+// @lc code=end
