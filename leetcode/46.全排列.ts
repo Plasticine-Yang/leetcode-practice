a0179dce8c2544dfbc26a4f89b98d50063a3599c
plasticine9750 <975036719@qq.com>
2022-06-08 16:09:54 +0800

    problem: 全排列

--- leetcode/46.全排列.ts
+++ leetcode/46.全排列.ts
@@ -9,30 +9,37 @@
 function permute(nums: number[]): number[][] {
   const res: number[][] = [];
   const track: number[] = [];
-  backtrack(nums, track, res);
+  const used: boolean[] = new Array(nums.length);
+
+  const backtrack = () => {
+    // base case
+    if (track.length === nums.length) {
+      res.push([...track]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      // track 中已经存在的数字就不需要加入了
+      if (used[i]) continue;
+
+      used[i] = true;
+      track.push(nums[i]);
+
+      backtrack();
+
+      used[i] = false;
+      track.pop();
+    }
+  };
+
+  backtrack();
 
   return res;
 }
 
-function backtrack(nums: number[], track: number[], res: number[][]) {
-  if (track.length === nums.length) {
-    // 路径已经完整了 可以加入到结果中
-    res.push([...track]); // 要拷贝 track 数组 否则放入的只是引用 会被下一次回溯修改
-    return;
-  }
-
-  // 回溯算法框架
-  for (const num of nums) {
-    // 排除不合法选择
-    if (track.includes(num)) continue;
-
-    // 做选择
-    track.push(num);
-    // 回溯
-    backtrack(nums, track, res);
-    // 撤销选择
-    track.pop();
-  }
-}
 // @lc code=end
+
+const ans = permute([1, 2, 3]);
+console.log(ans);
+
 export {};
